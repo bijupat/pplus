95c31eb17335e3542ff4755697857804afd177ac
Brijesh Patel <bijupat@users.noreply.github.com>
2021-10-23 00:03:05 +0530

    ADD PAY AND USER LOGIN IMPLEMENTED
    add pay and user log in implemented

--- report/urls.py
+++ report/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('index/', views.index, name = 'index'),
     path('encounter/<int:labkey>/', views.encounter, name='encounter'),
     path('report/<int:repokey>', views.report, name='report'),
     path('discount/<int:labkey>', views.discount, name='discount'),
     path('addpayment/<int:labkey>', views.addpayment, name='addpayment'),
+    path("", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
    
 
 
--- report/views.py
+++ report/views.py
@@ -1,29 +1,45 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tbllab, Tblinv, Tblrepo, Tbltests, Tblpay, Tblbills
+from .models import Tbllab, Tblinv, Tblrepo, Tbltests, Tblpay, Mstopr
 from django.utils.timezone import datetime
-from django.db.models import Sum, Max
+from django.db.models import Sum
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from datetime import timedelta
+from django.db import IntegrityError
 
+#time delta for 5 hours and 30 min
 TIME_DELTA530 = timedelta(days= 0, hours = 5, minutes = 30)
-
+#initialise empty USER set
+USERS=[]
+#Getting all mstopr object with only five colums 'oprkey', 'oprid', 'oprname', 'pw', 'active'
+USEROBJS = Mstopr.objects.all().values('oprkey', 'oprid', 'oprname', 'pw', 'active')
+#adding users in USERS set
+for USER in USEROBJS:
+    USERS.append(USER['oprid'])
+ACTIVEUSER =[]
 
 def index(request):
-    if request.method == 'GET':
-        #date format (yyyy,mm,dd)
+    if request.method == 'GET' and ACTIVEUSER:
         encounter_today = Tbllab.objects.filter(dor__date=datetime.today().date())
-        return render(request, 'report\index.html', {"encounter" :encounter_today})
+        return render(request, 'report/index.html' , {"encounter" :encounter_today, "user":ACTIVEUSER[0][0]})
+ 
 
-    elif request.method == 'POST':
+    elif request.method == 'POST' and ACTIVEUSER:
         date = request.POST["date"]
         encounter_date = Tbllab.objects.filter(dor__date=date)
-        return render(request, 'report\index.html', {"encounter" :encounter_date, "date" : date})
+        return render(request, 'report\index.html', {"encounter" :encounter_date, "date" : date, "user":ACTIVEUSER[0][0]})
+        
+    else:
+        return render(request, "report/login.html", {
+                "message": "Please Login."
+            })
+ 
 
-    
+
 def encounter(request, labkey):
-    if request.method == "POST":
+    if request.method == "POST" and ACTIVEUSER:
         repokey = request.POST.get('repokey')
         reportpg = Tblrepo.objects.get(pk=repokey)
         labkey = reportpg.labkey.labkey
@@ -34,7 +50,7 @@ def encounter(request, labkey):
 
         return HttpResponseRedirect(reverse("encounter",  args=[labkey]))
 
-    else:
+    elif request.method == "GET" and ACTIVEUSER:
         e = Tbllab.objects.get(pk=labkey)
         investigation = Tblinv.objects.filter(labkey=labkey)
         p = investigation.all().aggregate(Sum('rate'))
@@ -47,8 +63,13 @@ def encounter(request, labkey):
             due = total  - (int(total_payment) + int(e.disc))
         else:
             due = 0       
+        print(ACTIVEUSER[0])
+        return render(request, 'report\encounter.html', {"e" : e, "total": total, "investigations":investigation, "reports":reports, "payments" : payments, "total_payment":total_payment, "due":due, "user":ACTIVEUSER[0][0] } )
 
-        return render(request, 'report\encounter.html', {"e" : e, "total": total, "investigations":investigation, "reports":reports, "payments" : payments, "total_payment":total_payment, "due":due } )
+    else:
+        return render(request, "report/login.html", {
+                "message": "Please Login."
+            })
 
 def report(request, repokey):
 
@@ -71,8 +92,9 @@ def report(request, repokey):
         reporttitle = tests[0].repokey.title
         e = Tbllab.objects.get(pk=labkey)
 
-        return render(request, 'report\pgrep.html', {"tests" : tests, "e" : e , "reporttitle" :reporttitle, "repokey":repokey })
-    
+        return render(request, 'report\pgrep.html', {"tests" : tests, "e" : e , "reporttitle" :reporttitle, "repokey":repokey,"user":ACTIVEUSER[0][0] })
+
+
 def discount (request, labkey):
 
     if request.method == "POST":
@@ -81,7 +103,8 @@ def discount (request, labkey):
         e.save()
 
         return HttpResponseRedirect(reverse("encounter",  args=[labkey]))
-              
+
+
 def addpayment (request, labkey):
     if request.method == "POST":
         #getting latest Tblpay object sorted by paidon field lookup for recpno field
@@ -96,4 +119,37 @@ def addpayment (request, labkey):
         new_pay = Tblpay.objects.create(labkey=e, paidon = paidon, recpno=recpno, amount=amount, cash = True, printed= False, oprkey = 5, paymode = 0)
         
         return HttpResponseRedirect(reverse("encounter",  args=[labkey]))
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # getting vaues from post form
+        username = request.POST["username"]
+        password = request.POST["password"]
+        #cheking if username in global variable USERS set line 16
+        if username in USERS:
+            userobj = Mstopr.objects.filter(oprid=username)
+            pw = userobj[0].pw
+            # cheking pw and if user is active
+            if pw == password and userobj[0].active:
+                #appending global variable ACTIVEUSER set with current  validated usreobject
+                ACTIVEUSER.append(userobj)
+                encounter_today = Tbllab.objects.filter(dor__date=datetime.today().date())
+                return render(request, 'report/index.html' , {"encounter" :encounter_today, "user":userobj[0]})
+            else:
+                return render(request, "report/login.html", {
+                "message": "Invalid Password."
+            })
+        else:
+                return render(request, "report/login.html", {
+                "message": "Invalid Username."
+            })
+    else:
+        return render(request, "report/login.html")
+
+
+def logout_view(request):
+    ACTIVEUSER=[]
+    return HttpResponseRedirect(reverse("login"))
 
