920c13d820016eeeeb8d2468e7f5ae9ac6a13f81
Brijesh Patel <bijupat@users.noreply.github.com>
2021-11-25 20:09:47 +0530

    myweb app made as hospimax home site
    my web app
    views  and created..
    layout . html created
    href changed
    all html changed to server static and extend layout

---
+++ myweb/static/myweb/extend.js
@@ -0,0 +1,38 @@
+document.write('\
+\
+<div class="topfloat">\
+<div class="container">\
+  <div class="image"><img src="HOSPIMAX.jpg" id="imagehead" alt="Hospimax Consultancy"  title=" Hospimax Consultancy"></div>\
+  <div>\
+\
+<ul class="topnav">\
+  <li><a class="active" href="index.html">Home</a></li>\
+  <li class="dropdown">\
+    <a href="javascript:void(0)" class="dropbtn">Services</a>\
+    <div class="dropdown-content">\
+      <a href="user-defined.html">Custom healthcare software for healthcare units</a>\
+      <a href="appdev.html">Application Development & Support</a>\
+      <a href="intauto.html">Intelligent Automation in Healthcare</a>\
+      <a href="workimp.html">Custom Diagnostics center management system </a>\
+      <a href="telehealth.html">Tele-health & Virtual Health system development</a>\
+      <a href="website.html">Website and web application development</a>\
+      <a href="qa.html">Quality assurance for new or existing healthcare system</a>\
+    </div>\
+  </li>\
+  <li class="dropdown">\
+    <a href="javascript:void(0)" class="dropbtn">Our Team</a>\
+    <div class="dropdown-content">\
+      <a href="error.html">Promoter</a>\
+      <a href="error.html">Core Team</a>\
+    </div>\
+  </li>\
+  \
+  <li class="right"><a href="inquiry.html">Send Inquiry</a></li>\
+  <li class="right"><a href="contactus.html">Contact Us</a></li>\
+</ul>\
+</div>\
+</div>\
+</div>\
+\
+\
+');
--- myweb/views.py
+++ myweb/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-
-    #return HttpResponse("Hello, world. You're at the polls index.")
-                
-    return render(request, 'myweb\index.html')
-
+def index(request):                
+    return render(request, 'myweb/index.html')
  
+def user_defined(request):
+    return render(request, 'myweb/user_defined.html')
+ 
+def appdev(request):
+    return render(request, 'myweb/appdev.html')
+ 
+def intauto(request):
+    return render(request, 'myweb/intauto.html')
+
+def workimp(request):
+    return render(request, 'myweb/workimp.html')
+
+def telehealth(request):
+    return render(request, 'myweb/telehealth.html')
+
+def website(request):
+    return render(request, 'myweb/website.html')
+
+def qa(request):
+    return render(request, 'myweb/qa.html')
+
+
+def promotor(request):
+    return render(request, 'myweb/error.html')
+        
+
+def coreteam(request):
+    return render(request, 'myweb/error.html')
+        
+def inquiry(request):
+    return render(request, 'myweb/inquiry.html')
+        
+def contactus(request):
+    return render(request, 'myweb/contactus.html')
